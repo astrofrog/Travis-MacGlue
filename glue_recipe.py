06a079567979713b9adca40ef4ca5df7199c0e8b
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-07-02 19:23:35 +0100

    Added a patch for entry points

--- glue_recipe.py
+++ glue_recipe.py
@@ -1,10 +1,7 @@
-#ugly hacking to repoint py2app at the right icon, ui files
-
-from cStringIO import StringIO
-
+# Ugly hacking to repoint py2app at the right icon, ui files
 patch = """
 import os
-from glue.qt import ui, icons, qtutil
+from glue.qt import icons, qtutil
 
 def icon_path(icon_name):
     if not icon_name.endswith('.png'):
@@ -15,5 +12,43 @@ def icon_path(icon_name):
 
 qtutil.icon_path = icon_path
 """
+
+# We have to manually get the entry points and patch them in glue because py2app
+# doesn't support entry points
+
+import pkg_resources
+
+# List all bundled plugin packages here
+PACKAGES = ['glueviz']
+
+# list_plugin_entry_points
+
+patch += """
+from pkg_resources import EntryPoint
+
+def iter_plugin_entry_points():
+"""
+
+for package in PACKAGES:
+
+    dist = pkg_resources.working_set.by_key[package]
+
+    entry_map = dist.get_entry_map()
+
+    entry_points_str = ""
+
+    for key, entry_points in entry_map.items():
+        if key == 'glue.plugins':
+            for name, entry_point in entry_points.items():
+                patch += "    yield EntryPoint('{name}', '{module_name}', attrs={attrs})\n".format(name=name, module_name=entry_point.module_name, attrs=entry_point.attrs)
+
+patch += """
+from glue import main
+main.iter_plugin_entry_points = iter_plugin_entry_points
+"""
+
+print(patch)
+
 def check(cmd, mg):
+    from cStringIO import StringIO
     return dict(prescripts=[StringIO(patch)])
